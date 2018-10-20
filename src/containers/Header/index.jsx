f74da5ed70552cb9fe26cb4f3a29c9c21f1264cf
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-10-20 21:17:18 +0600

    header desgin and component add

---
+++ src/containers/Header/index.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router'
+import styled from 'styled-components';
+
+const ProfileImage = styled.img`
+  width: 12em;
+  height: 12em;
+  border-radius: 50%;
+`;
+
+const ProfileTitle = styled.h1`
+  color: black;
+`
+
+const HeaderDiv = styled.div`
+  padding-top: 5vh;
+  text-align: center;
+  font-family: 'Zilla Slab Highlight', sans-serif;
+  color: black;
+`;
+
+const NavLink = styled(Link)`
+  display: inline-flex;
+  margin: 0 5px;
+  color: black;
+  font-size: 16px;
+`
+
+
+class Header extends Component {
+  render() {
+    return (
+      <HeaderDiv>
+        <Link to="/">
+          <ProfileImage src={require('../assets/confettiCropped.jpg')} alt='Sophia Ciocca'/>
+          <ProfileTitle>Mirajul Karim Wasif</ProfileTitle>
+        </Link>
+        <div>
+          <NavLink to='/'>ABOUT</NavLink>
+          <NavLink to='/code'>CODE</NavLink>
+          <NavLink to='/writing'>BLOG</NavLink>
+          <NavLink to='/life'>LIFE</NavLink>
+          <NavLink to='/contact'>CONTACT</NavLink>
+        </div>
+        <hr/>
+      </HeaderDiv>
+    );
+  }
+}
+
+export default Header;
