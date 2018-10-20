85e2638efa448efc9d9b5021562d73f20e80339c
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-10-20 21:25:49 +0600

    update app content

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import Header from './Header.js'
+
+export const ColorScheme = {
+  primary: 'red',
+  secondary: 'blue',
+  third: 'purple',
+  fourth: 'goldenrod'
+}
+
+const AppDiv = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  font-size: 1.15em;
+`;
+
+export const CenteredHeader = styled.h4`
+  text-align: center;
+  margin-bottom: 1.25em;
+`
+
+
+const ContainerDiv = styled.div`
+  margin: 1% 17% 3% 17%;
+  flex: 1;
+  font-family: 'Open Sans', sans-serif;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>app</h1>
-      </div>
+      <AppDiv>
+        <Header />
+        <ContainerDiv>
+          {this.props.children}
+        </ContainerDiv>
+        <Footer />
+      </AppDiv>
     );
   }
 }
