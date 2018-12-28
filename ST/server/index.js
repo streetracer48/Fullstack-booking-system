ee0f5bd95aa82270123b79fc130c24a64d5c2059
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-12-28 10:50:22 +0600

    connecting to mlab server

--- ST/server/index.js
+++ ST/server/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config =require('../config/dev')
+mongoose.connect(config.DB_URI);
 const app = express();
+
 
 const port = process.env.PORT || 3002;
 
