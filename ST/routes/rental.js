03793ab86e5a7e3396752bf202f7674197c6ef7e
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-12-28 12:04:14 +0600

    get rental by id

--- ST/routes/rental.js
+++ ST/routes/rental.js
@@ -12,6 +12,21 @@ router.get('', function(req,res){
         res.status(200).send(docs) 
   })
 
-})
+});
+
+
+router.get('/:id', function(req,res){
+
+    const rentalId = req.params.id;
+
+    Rental.findById(rentalId, (err, docs) => {
+        if(err) {
+              return res.status(400).send(err);
+        }
+        res.status(200).send(docs) 
+  })
+
+});
+
 
 module.exports = router;
