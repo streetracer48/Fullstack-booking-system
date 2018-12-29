c6fc2253729327619b8f1ae5efc64215387bc6a3
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-12-29 23:42:45 +0600

    creating user models

---
+++ ST/models/user.js
@@ -0,0 +1,32 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    min: [4, 'Too short, min is 4 characters'],
+    max: [32, 'Too long, max is 32 characters']
+  },
+  email: {
+    type: String,
+    min: [4, 'Too short, min is 4 characters'],
+    max: [32, 'Too long, max is 32 characters'],
+    unique: true,
+    lowercase: true,
+    required: 'Email is required',
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+  },
+  password: {
+    type: String,
+    min: [4, 'Too short, min is 4 characters'],
+    max: [32, 'Too long, max is 32 characters'],
+    required: 'Password is required'
+  },
+  stripeCustomerId: String,
+  revenue: Number,
+  rentals: [{type: Schema.Types.ObjectId, ref: 'Rental'}],
+  bookings: [{ type: Schema.Types.ObjectId, ref: 'Booking' }]
+});
+
+module.exports = mongoose.model('User', userSchema );
